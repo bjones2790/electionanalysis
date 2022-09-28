cb23f12ccb34cbf7c7e3288db7cdcf2332198807
bjones2790 <bjones.2790@gmail.com>
2022-09-28 15:35:09 -0400

    Adding the election code

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -80,12 +80,3 @@ for candidate_name in candidate_votes:
 print(winning_candidate_summary)
 
 
-#1. The total number of votes cast
-
-#2. A complete list of candidates who received votes
-
-#3. The % of votes each candidate won
-
-#4. The total number of votes each candidate won
-
-#5. The winner of the election based on popular vote
